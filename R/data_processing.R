c60b1cf828e4eec715aa65c2c0c5b5a4f76aece3
JayaniLakshika <jayanilakshika76@gmail.com>
2024-05-29 11:07:12 +1000

    Look at the list

--- R/data_processing.R
+++ R/data_processing.R
@@ -19,4 +19,24 @@ response <- VERB("GET", url, query = queryString, content_type("application/octe
 value <- content(response, "parsed")
 
 content(response, "parsed")[[2]] ## Go to response
-content(response, "parsed")[[2]][1] ## Go to a location (should goes from 1 to 100)
+content(response, "parsed")[[2]][1] ## Go to a location (should goes from 1 to 100). This should be pass as a paramater 100
+
+data_list <- content(response, "parsed")[[2]][1][[1]] ## Go inside the list
+
+tibble(location_id = data_list$id,
+       city = data_list$city,
+       name = data_list$name,
+       country = data_list$country,
+       lat = data_list$coordinates$latitude,
+       long = data_list$coordinates$longitude
+       )
+
+
+param_list <- data_list$parameters
+tibble(param_id = param_list[[1]]$id,
+       name = param_list[[1]]$parameter,
+       unit = param_list[[1]]$unit,
+       count = param_list[[1]]$count,
+       average = param_list[[1]]$average,
+       last_updated = param_list[[1]]$lastUpdated
+)
