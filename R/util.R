70576281ae352d8613056512d619ef7d2ba224c4
Michael Lydeamore <michael.lydeamore@monash.edu>
2024-05-29 21:30:58 +1000

    Fix null input parsing

--- R/util.R
+++ R/util.R
@@ -1,7 +1,13 @@
 restify_vector <- function(input, name = NULL) {
+    if (is.null(input)) {
+        return(NULL)
+    }
     if (is.null(name)) {
         name <- deparse(substitute(input))
     }
+    input <- sapply(input, function(s) {
+        gsub(" ", "%20", s)
+    })
     string <- paste0("&", name, "=")
 
     paste0(input, collapse=string)
