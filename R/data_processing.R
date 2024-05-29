13c504a38c052dfe9baf74ac29514cf1b36d475f
JayaniLakshika <jayanilakshika76@gmail.com>
2024-05-29 14:21:51 +1000

    Update data_processing.R

--- R/data_processing.R
+++ R/data_processing.R
@@ -2,51 +2,26 @@ library(tibble)
 library(dplyr)
 library(purrr)
 
-url <- "https://api.openaq.org/v2/locations"
-
-queryString <- list(
-  limit = "100",
-  page = "1",
-  offset = "0",
-  sort = "desc",
-  radius = "1000",
-  order_by = "lastUpdated",
-  dump_raw = "false"
-)
-
-response <- VERB("GET", url, query = queryString, content_type("application/octet-stream"), accept("application/json"))
-
-value <- content(response, "parsed")
-
 get_data <- function(value) {
 
   # To obtain the response
   locations_list <- value[[2]]
 
   # Convert locations_list to tibble
-  location_tibble <- map_dfr(locations_list, ~ tibble(
-    location_id = .x$id,
-    city = .x$city,
-    name = .x$name,
-    country = .x$country,
-    lat = .x$coordinates$latitude,
-    long = .x$coordinates$longitude
-  ))
-
-  # Convert parameters to tibble and unnest
-  param_tibble <- locations_list |>
-    map(~ tibble(
-      location_id = .x$id,
-      param_name = .x$parameters |> map_chr("parameter"),
-      unit = .x$parameters |> map_chr("unit"),
-      count = .x$parameters |> map_dbl("count"),
-      average = .x$parameters |> map_dbl("average"),
-      last_updated = .x$parameters |> map_chr("lastUpdated")
-    )) |>
-    bind_rows()
-
-  # Join location and parameter data
-  air_df <- inner_join(location_tibble, param_tibble, by = "location_id")
+  # Extract data from locations_list and combine into air_df
+  air_df <- locations_list |>
+    map_dfr(~ tibble(
+      location_id = .x$locationId,
+      location = .x$location,
+      parameter = .x$parameter,
+      value = .x$value,
+      date_utc = .x$date$utc,
+      unit = .x$unit,
+      lat = .x$coordinates$latitude,
+      long = .x$coordinates$longitude,
+      country = .x$country,
+      city = .x$city
+    ))
 
   return(air_df)
 
